259ed149314d4f4522819ca53f4278c4cc9bdddb
ClementSellyPro <clementselly.pro@gmail.com>
2024-05-07 20:54:13 +0400

    Little update CSS about the design

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const projectList = document.querySelector('.projectList');
 
 projectList.addEventListener('wheel', (e) => {
-    if(window.innerWidth > 720){
-        projectList.scrollLeft += e.deltaY;
+    if(window.innerWidth > 1000){
+        projectList.scrollLeft += e.deltaY ;
     }
 });
