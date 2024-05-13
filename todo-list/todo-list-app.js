d0d0afdbc0798aee15f5d3e7557e46139693a449
ClementSellyPro <clementselly.pro@gmail.com>
2024-05-13 19:33:35 +0400

    todo-list - Continue the js file

--- todo-list/todo-list-app.js
+++ todo-list/todo-list-app.js
@@ -1,3 +1,9 @@
+/* --------------- VARIABLES INITIALIZATION --------------- */
+let taskList = ['Manger', 'Boire', 'Lire des lives'];
+
+// 'Manger', 'Boire', 'Lire des lives'
+
+
 /* --------------- DOM ELEMENT SELECTIONS --------------- */
 let body = document.querySelector('body');
 let themeBtn = document.querySelector('.icon-theme');
@@ -6,26 +12,12 @@ let input = document.querySelector('.input-field')
 let addBtn = document.querySelector('.add-icon');
 
 let todoListDOM = document.querySelector('.todo-list');
-
-/* --------------- VARIABLES INITIALIZATION --------------- */
-let taskList = [];
+let allTaskElement = [todoListDOM.querySelectorAll('.todo-element')];
 
 
-
-/* ---------------         EVENTS         --------------- */
-function getInput(){
-    return input.value;
-}
-
-function addNewTask(){
-    let currentInput = getInput();
-    taskList.push(currentInput);
-    
-    console.log(taskList);
-    input.value = '';
-}
-
+/* ---------------         FUNCTIONS        --------------- */
 function noTaskMessage(){
+    todoListDOM.innerHTML = '';
     if(taskList.length === 0){
         let noTaskMessageBox = document.createElement('div');
         noTaskMessageBox.setAttribute('class', 'noTaskMessageBox');
@@ -34,15 +26,49 @@ function noTaskMessage(){
         
         noTaskMessageBox.appendChild(noTaskMessageTxt);
         todoListDOM.appendChild(noTaskMessageBox);
-        console.log('yes');
     }
 }
+// add a new task from the input field
+function addNewTask(){
+    if(input.value !== ''){
+        taskList.push(input.value);
+    }
+    //reset input field
+    input.value = '';
+}
+// display all task on the list
+function displayTaskList(){
+    todoListDOM.innerHTML = '';
+    for(let i = 0; i < taskList.length; i++){
+        let todoElement = document.createElement('div');
+        todoElement.setAttribute('class', 'todo-element');
 
+        // let checkbox = document.createElement('div');
+        // checkbox.setAttribute('class', 'checkbox');
+        todoElement.innerHTML = `
+            <div class="checkbox"></div> 
+            <p> ${taskList[i]} </p>
+            <div class="deleteTask"><img src="images/icon-cross.svg" alt=""></div>
+        `
+        todoListDOM.appendChild(todoElement);
+    }
+}
+// remove a task from the list
+function removeTask(){
+    console.log('yes');
+}
+// update the display function
 function updateDisplay(){
     noTaskMessage();
+    if(taskList.length > 0){
+        displayTaskList();
+    }
+    //refresh the array of task
+    allTaskElement = document.querySelectorAll('.todo-element');
 }
 
 /* ---------------         EVENTS         --------------- */
+// change the theme
 themeBtn.addEventListener('click', () => {
     if(body.classList.contains('dark')){
         body.classList.remove('dark');
@@ -52,12 +78,11 @@ themeBtn.addEventListener('click', () => {
         themeBtn.src = 'images/icon-sun.svg';
     }
 });
-
+// add new task onClik and onKeypress
 addBtn.addEventListener('click', () => {
     addNewTask();
     updateDisplay();
 });
-
 input.addEventListener('keypress', (event) => {
     if(event.key === 'Enter'){
         addNewTask();
@@ -65,6 +90,6 @@ input.addEventListener('keypress', (event) => {
     }
 })
 
-window.addEventListener('load', () => {
-    updateDisplay();
-});
+
+
+window.addEventListener('load', () => updateDisplay());
