668c80e7e09a0fb84aac8d6280923a9d870c1068
ClementSellyPro <clementselly.pro@gmail.com>
2024-05-24 21:08:13 +0400

    multi-step-form - Finish all style and JS navigation between page () modal

--- multi-step-form-main/app.js
+++ multi-step-form-main/app.js
@@ -3,6 +3,8 @@
 let modalOne = document.querySelector('.step-one-modal');
 let modalTwo = document.querySelector('.step-two-modal');
 let modalThree = document.querySelector('.step-three-modal');
+let modalFour = document.querySelector('.step-four-modal');
+let modalFive = document.querySelector('.step-five-modal');
 
 
 
@@ -10,11 +12,14 @@ let modalThree = document.querySelector('.step-three-modal');
 let btnNextOne = document.querySelector('.btn-next-step-one');
 let btnBackTwo = document.querySelector('.btn-back-step-two');
 let btnNextTwo = document.querySelector('.btn-next-step-two');
+let btnBackThree = document.querySelector('.btn-back-step-three');
+let btnNextThree = document.querySelector('.btn-next-step-three');
+let btnBackFour = document.querySelector('.btn-back-step-four');
+let btnConfirm = document.querySelector('.btn-confirm');
 
 
 
-
-/* --------------------      FUNTIONS       -------------------- */
+/* --------------------      FUNCTIONS       -------------------- */
 
 
 
@@ -37,3 +42,19 @@ btnNextTwo.addEventListener('click', () => {
     modalTwo.classList.add('hidden');
     modalThree.classList.remove('hidden');
 });
+// page 3 to page 2
+btnBackThree.addEventListener('click', () => {
+    modalThree.classList.add('hidden');
+    modalTwo.classList.remove('hidden');
+
+})
+// page 3 to page 4
+btnNextThree.addEventListener('click', () => {
+    modalThree.classList.add('hidden');
+    modalFour.classList.remove('hidden');
+})
+// page 4 to page 3
+btnBackFour.addEventListener('click', () => {
+    modalFour.classList.add('hidden');
+    modalThree.classList.remove('hidden');
+})
