7727cc468d6c70495b60cb11303611ba5f2f9d6d
ClementSellyPro <clementselly.pro@gmail.com>
2024-08-17 10:52:23 +0400

    product-list - Delete an item in cart Section + update display, display modal Confirmation Order but not update when deletint an item

--- product-list-with-cart-main/app.js
+++ product-list-with-cart-main/app.js
@@ -1,6 +1,6 @@
 /* -------------------- Variables init   -------------------- */
 let orderList = [];
-
+let totalOrder = 0;
 
 /* -------------------- DOM element      -------------------- */
 const cardsSection = document.querySelector('.cards-section');
@@ -8,7 +8,15 @@ const emptyCart = document.querySelector('.cart__empty');
 const filledCart = document.querySelector('.cart__filled');
 const filledCartItems = document.querySelector('.cart__filled-items');
 const cartItemsNumber = document.querySelector('.cart-items-number');
+const totalOrderCart = document.querySelector('.cart__filled-row-total-price');
+const cartConfirmBtn = document.querySelector('.cart__confirm-btn');
 
+const modalContainer = document.querySelector('.modal-container');
+const modal = document.querySelector('.modal');
+const modalOrderSection = document.querySelector('.modal__order-section');
+const totalOrderModal = document.querySelector('.modal__order-total-price');
+
+const startNewOrderBtn = document.querySelector('.modal__confirm-btn');
 
 /* -------------------- Functions       -------------------- */
 // function to display all the food card
@@ -36,39 +44,52 @@ function displayCards(data){
         cardsSection.appendChild(currentCard);
     }
 }
+
 // function to add an item in the list (orderList array)
 function addNewItem(id, data){
-    orderList.push(data[id]);
+    let newItem = {...data[id], 'id':id};
+    orderList.push(newItem);
 }
+
 // check if orderList is empty or not, to display the empty message or the list
 function isCartEmpty(orderList){
     if(orderList.length === 0){
         if(filledCart.classList.contains('hidden') || emptyCart.classList.contains('hidden')){
             filledCart.classList.add('hidden');
             emptyCart.classList.remove('hidden');
-            console.log('orderList vide');
         }
     }else{
         if(emptyCart.classList.contains('hidden') || filledCart.classList.contains('hidden')){
             emptyCart.classList.add('hidden');
             filledCart.classList.remove('hidden');
-            console.log('orderList pas trop vide');
         }
     }
 }
+
+// define the total order 
+function defineTotal(orderList){
+    totalOrder = 0;
+    for(let i = 0; i < orderList.length; i++){
+        totalOrder += Number(orderList[i].price);
+    }
+    totalOrderCart.innerHTML = `$${totalOrder}`;
+    totalOrderModal.innerHTML = `$${totalOrder}`;
+}
+
 // display the orderList in the Cart section
 function displayCartItems(orderList){
     filledCartItems.innerHTML = '';
     for(let i = 0; i < orderList.length; i++){
         let orderListItem = document.createElement('div');
         orderListItem.setAttribute('class', 'cart__filled-row');
+        orderListItem.setAttribute('data-id', `${orderList[i].id}`)
 
         orderListItem.innerHTML = `
           <div class="cart__filled-title">${orderList[i].name}</div>
           <div class="cart__filled-details">
-            <div class="cart__filled-amount">2x</div>
+            <div class="cart__filled-amount">1x</div>
             <div class="cart__filled-price">@ $${orderList[i].price}</div>
-            <div class="cart__filled-total-price">$11.00</div>
+            <div class="cart__filled-total-price">$${orderList[i].price}</div>
           </div>
           <img class="cart__delete-btn" src="/assets/images/icon-remove-item.svg" alt="Delete button" />`;
 
@@ -76,6 +97,46 @@ function displayCartItems(orderList){
     }
     cartItemsNumber.innerHTML = orderList.length;
 }
+
+// delete selected item
+function deleteItem(orderList, id){
+    let index = 0;
+    for(let i = 0; i < orderList.length; i++){
+        if(orderList[i].id === id){
+            index = i;
+        }
+    }
+    orderList.splice(index, 1);
+    displayCartItems(orderList);
+    displayModalItems(orderList);
+}
+
+// display the orderList in the Cart section
+function displayModalItems(orderList){
+    const modalItem = document.createElement('div');
+    modalItem.setAttribute('class', 'modal__order');
+
+    for(let i = 0; i < orderList.length; i++){
+        modalItem.innerHTML = `
+            <img class="modal__img" src="${orderList[i].image.thumbnail}" alt="food"/>
+            <div class="modal__details">
+            <p class="modal__row-title">${orderList[i].name}</p>
+            <p class="modal__row-price"><span>x1</span>  @ $${orderList[i].price}</p>
+            </div>
+            <span class="modal__row-total-item">$${orderList[i].price}</span>`;
+
+        modalOrderSection.appendChild(modalItem);
+    }
+}
+
+/* -------------------- EVENT       -------------------- */
+// display and hide modal modal
+cartConfirmBtn.addEventListener('click', () => {
+    modalContainer.style.display = 'flex';
+});
+startNewOrderBtn.addEventListener('click', () => {
+    modalContainer.style.display = 'none';
+})
 
 
 /* -------------------- EXECUTION       -------------------- */
@@ -100,8 +161,20 @@ async function execution(){
             // check if orderList is empty
             isCartEmpty(orderList);
             console.log(orderList);
-            // display orderList items in Cart section
+            // display orderList items in Cart section, and in modal section
+            defineTotal(orderList);
             displayCartItems(orderList);
+            displayModalItems(orderList);
+
+            // collect all delete button
+            const deleteBtn = document.querySelectorAll('.cart__delete-btn');
+            // event click delete item in cart section
+            for(let j = 0; j < deleteBtn.length; j++){
+                deleteBtn[j].addEventListener('click', (e) => {
+                    let targetID = e.target.parentElement.dataset.id;
+                    deleteItem(orderList, targetID);
+                });
+            }
         });
     }
 }
